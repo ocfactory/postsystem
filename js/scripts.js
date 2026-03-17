e4b71227d4b92888eb1b8916282a3cdee61cd608
OC <62410394+ocfactory@users.noreply.github.com>
2026-03-17 17:55:46 +0900

    Change default language from 'en' to 'kr'

--- js/scripts.js
+++ js/scripts.js
@@ -322,7 +322,7 @@ window.addEventListener('DOMContentLoaded', event => {
 	});
 	
 	// 저장된 언어 불러오기
-	const savedLang = localStorage.getItem("language") || "en";
+	const savedLang = localStorage.getItem("language") || "kr";
 
 	setLanguage(savedLang);
 
